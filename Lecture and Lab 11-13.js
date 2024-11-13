ab97d2db890eff255a02cee2826c4f71509d25aa
eja37 <eja37@njit.edu>
2024-11-13 12:44:43 -0500

    Q2

--- Lecture and Lab 11-13.js
+++ Lecture and Lab 11-13.js
@@ -1,5 +1,15 @@
 let Firstname = "Edna";
 console.log(Firstname);
+
 let age = 20;
 console.log(age);
 
+let food = "Sandwhich";
+console.log(food);
+
+let number = 100;
+if (number > 40){
+    house = true
+    console.log(number)
+    console.log(house)
+}
