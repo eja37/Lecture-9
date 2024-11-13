46f738615bfb4f9165976a500ea5add8714ac0af
eja37 <eja37@njit.edu>
2024-11-13 11:36:47 -0500

    lecture

---
+++ scriptone.js
@@ -0,0 +1,21 @@
+var age = 20;
+
+var age;
+
+age = 100;
+
+
+let LastName = "Harry";
+
+LastName = "Potter";
+
+let FirstName = "Edna Janet";
+
+
+console.log(FirstName + "Edna Janet"+LastName)
+
+const valuePi = 3.14;
+
+valuePi = 3.14;
+
+console.log(valuePi);
