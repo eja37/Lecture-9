5af2cb75a277768cd2f905e3ad4175284bcfcd96
eja37 <eja37@njit.edu>
2024-11-25 09:49:18 -0500

    update

--- scriptone.js
+++ scriptone.js
@@ -38,3 +38,28 @@ console.log(`Hello, ${Firstname} ${Lastname}`);
 let book = "Harry Potter";
 
 console.log( book.replace("Potter", "Gryffindor"));
+
+
+function AddNumbers(){
+    let a = 10;
+    let b = 100;
+    console.log(a+b);
+    document.getElementById('heading-two').innerText = (a+b);
+    document.getElementById('para1').innerHTML = '<h1>This is final sum</h1?';
+}
+
+document.getElementById('btn1').addEventListener('click', AddNumbers);
+
+let firstname = 'Harry'; 
+let pass = 'potter';
+
+document.getElementById('form1').addEventListener('submit', function(event)){
+    if(document.getElementById('username').value == firstname && document.getElementById('password').value == password){
+        console.log('login success')
+    }
+else{
+    event.preventDefault();
+}
+}
+
+document.querySelector('.div1 #id1')
