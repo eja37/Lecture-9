693d90a7d34636661f68e1e58717cee5e89fc3a5
eja37 <eja37@njit.edu>
2024-12-02 22:36:27 -0500

    answered all questions

--- Dec 2 Lab.js
+++ Dec 2 Lab.js
@@ -14,14 +14,9 @@ document.getElementById('btn1').addEventListener('click', changeText)
 
 //Q3//
 function addNumber(){
-    let inputNumber1 = document.getElementById('num1').value;
-    let inputNumber2 = document.getElementById('num2').value;
-
-    let sum = number(numb1) + number(numb2);
-
-    document.getElementById('result').innerText = "result: " + sum;
-
+    document.getElementById('result').innerText = Number(document.getElementById('num1').value) + Number(document.getElementById('num2').value);
 }
+document.getElementById('btn2').addEventListener('click',addNumber)
 
 //Q4//
 document.getElementById('btn3').onclick = function(){
@@ -40,3 +35,11 @@ document.getElementById('btn4').onclick = function(){
 function combineStrings(){
     document.getElementById('combinedText').innerText = document.getElementById('input1').value + document.getElementById('input2').value;
 }
+
+//Q6//
+
+function changeBackgroundColor(){
+    document.getElementById('colorbox').style.backgroundColor = "blue";
+}
+
+document.getElementById('change').addEventListener("click", changeBackgroundColor)
